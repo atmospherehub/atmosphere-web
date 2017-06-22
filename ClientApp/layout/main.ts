56e852a444551d4453f93bdd5451f8a17b78a6f4
Jenya Y <jenyay@gmail.com>
2017-06-22 13:43:41 +0300

    Switched CSS template

--- ClientApp/layout/main.ts
+++ ClientApp/layout/main.ts
@@ -1,7 +1,7 @@
 import { Aurelia, PLATFORM } from 'aurelia-framework';
 import { Router, RouterConfiguration } from 'aurelia-router';
 
-export class App {
+export class Main {
     router: Router;
 
     configureRouter(config: RouterConfiguration, router: Router) {
@@ -10,21 +10,14 @@ export class App {
             route: [ '', 'home' ],
             name: 'home',
             settings: { icon: 'home' },
-            moduleId: PLATFORM.moduleName('../home/home'),
+            moduleId: PLATFORM.moduleName('../components/home/home'),
             nav: true,
             title: 'Home'
-        }, {
-            route: 'counter',
-            name: 'counter',
-            settings: { icon: 'education' },
-            moduleId: PLATFORM.moduleName('../counter/counter'),
-            nav: true,
-            title: 'Counter'
         }, {
             route: 'fetch-data',
             name: 'fetchdata',
             settings: { icon: 'th-list' },
-            moduleId: PLATFORM.moduleName('../fetchdata/fetchdata'),
+            moduleId: PLATFORM.moduleName('../components/fetchdata/fetchdata'),
             nav: true,
             title: 'Fetch data'
         }]);
