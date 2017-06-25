c689b36872db6e4990c0c0d7d40bd46546d56e79
Jenya Y <jenyay@gmail.com>
2017-06-26 01:03:54 +0300

    Added loader

--- ClientApp/components/charts/base-chart.ts
+++ ClientApp/components/charts/base-chart.ts
@@ -10,6 +10,7 @@ export abstract class BaseChartCustomElement<T> {
     protected _http: HttpClient;
     protected _chart: Chart;
     protected _toolbarChanges: Subscription;
+    public isLoading: boolean;
 
     constructor(element: Element, http: HttpClient, toolbar: Toolbar) {
         this._element = element;
@@ -18,16 +19,22 @@ export abstract class BaseChartCustomElement<T> {
     }
 
     attached() {
+        this.isLoading = true;
         this._toolbarChanges = this._toolbar.subscribe(() => {
+            this.isLoading = true;
             this.getData(this._toolbar.range)
                 .then(data => {
                     this._chart.data = this.getBindingData(this._toolbar.moods, data);
                     this._chart.update(0);
+                    this.isLoading = false;
                 });
         });
 
         this.getData(this._toolbar.range)
-            .then(data => this._chart = this.createChart(this.getBindingData(this._toolbar.moods, data)));
+            .then(data => {
+                this._chart = this.createChart(this.getBindingData(this._toolbar.moods, data));
+                this.isLoading = false;
+            });
     }
 
     detached() {
@@ -38,5 +45,5 @@ export abstract class BaseChartCustomElement<T> {
 
     abstract getBindingData(moods: Mood[], data: T[]): any;
 
-    abstract createChart(bindingData: any) : Chart;
+    abstract createChart(bindingData: any): Chart;
 }
