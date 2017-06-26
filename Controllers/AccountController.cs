454c59d569d80165ed9c0b2aa3c31e907ebb4bc6
Jenya Y <jenyay@gmail.com>
2017-06-26 18:09:49 +0300

    Token endpoint

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.Authentication;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Security.Claims;
@@ -23,6 +24,12 @@ namespace AtmosphereWeb.Controllers
         public IActionResult Forbidden()
         {
             return View();
+        }
+
+        [Authorize(ActiveAuthenticationSchemes = "Cookie")]
+        public IActionResult GetBearerToken()
+        {
+            return new EmptyResult();
         }
 
         private IActionResult redirectLocal(string returnUrl)
