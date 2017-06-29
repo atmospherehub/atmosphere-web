537f6e4566c739b334f949d75a0d3c3a23e645b0
Jenya Y <jenyay@gmail.com>
2017-06-29 10:33:01 +0300

    Changning moods should not get data from API again

--- ClientApp/components/charts/base-chart.ts
+++ ClientApp/components/charts/base-chart.ts
@@ -5,6 +5,7 @@ import { Chart } from 'chart.js';
 import { Toolbar, DatesRange, Mood } from './../../services/toolbar';
 
 export abstract class BaseChartCustomElement<T> {
+    private _data: T[];
     protected _toolbar: Toolbar;
     protected _element: Element;
     protected _chart: Chart;
@@ -20,25 +21,39 @@ export abstract class BaseChartCustomElement<T> {
 
     attached() {
         this.isLoading = true;
-        this._toolbarChanges = this._toolbar.subscribe(() => {
-            this.isLoading = true;
-            this.getData(this._toolbar.range)
-                .then(data => {
-                    this._chart.data = this.getBindingData(this._toolbar.moods, data);
-                    this._chart.update(0);
-                    this.isLoading = false;
-                });
+        this._toolbarChanges = this._toolbar.subscribe((changeName: string) => {
+            if (changeName == 'moods' && this._data != null) {
+                // we don't refresh data when moods change,
+                // because `this._data` has a superset of moods
+                this._chart.data = this.getBindingData(this._toolbar.moods, this._data);
+                this._chart.update(0);
+            }
+            else {
+                this.isLoading = true;
+                this._chart.data = null;
+                this.getData(this._toolbar.range)
+                    .then(data => {
+                        this._data = data;
+                        this._chart.data = this.getBindingData(this._toolbar.moods, this._data);
+                        this._chart.update(0);
+                        this.isLoading = false;
+                    });
+            }
         });
 
         this.getData(this._toolbar.range)
             .then(data => {
-                this._chart = this.createChart(this.getBindingData(this._toolbar.moods, data));
+                this._data = data;
+                this._chart = this.createChart(this.getBindingData(this._toolbar.moods, this._data));
                 this.isLoading = false;
             });
     }
 
     detached() {
         this._toolbarChanges.dispose();
+        this._data = null;
+        if (this._chart != null)
+            this._chart.destroy();
     }
 
     abstract getData(range: DatesRange): Promise<T[]>;
